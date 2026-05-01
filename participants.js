2744a6642d01b6d08d14065bf05a50e0d7689d6d
Dolm Chen <34645580+whoareyou40@users.noreply.github.com>
2026-05-01 22:41:47 +0800

    Update: 功能优化

--- participants.js
+++ participants.js
@@ -3,14 +3,15 @@
 window.HL_DEFAULT_PARTICIPANTS = [
   { address: "0x8dd9bef9f99750d9b9f8433a0032d3ba8a492121", name: "wmsooooj" },
   { address: "0xb325e9fe36c0ca5a0862551d0310217e381a029b", name: "hh" },
-  { address: "0x7f8cf0edce270c61c6eabbaf81ef910bbcfc205f", name: "0xCrescendo" },
+  { address: "0x7F8CF0EdCE270c61c6EabbAF81Ef910bbBcF205F", name: "0xCrescendo" },
   { address: "0xdec8dea6c70e427fff0a4bb0b0b3de484b8d3763", name: "xx" },
-  { address: "0x42eb09a127301d5fb82647b80872e794c7930ffc", name: "是" },
+  { address: "0x42eB09A127301D5fB82647b80872e794c7930FFC", name: "是" },
   { address: "0x5d919b1c1f88443643f7152199d5fbe631246ca9", name: "song" },
   { address: "0x58d7670399b343a7bce263ce21704ba3a2879c63", name: "L神" },
   { address: "0xf605168cd88168b553fe89161ad22f74147c9d00", name: "MinerB" },
   { address: "0x4eb0eab5e543374b1f5faa586728a27c48c7b09e", name: "0xfree" },
   { address: "0x7effe5b3b66f2a6698e8e060cbc9aa36fd9763ee", name: "jim" },
-  { address: "0x7effe5b3b66f2a6698e8e060cbc9aa36fd9763ee", name: "newjim" }
+  { address: "0x7effe5b3b66f2a6698e8e060cbc9aa36fd9763ee", name: "newjim"},
+  { address: "0xc07A4756CD5d82dA0F1599cE9942FC49B6265f7d", name: "boss" } 
 ];
 
