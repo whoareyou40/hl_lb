931a8be8fda4785052f52dd8785f088560088bb8
Dolm Chen <34645580+whoareyou40@users.noreply.github.com>
2026-05-04 18:37:24 +0800

    Update: 功能优化

--- participants.js
+++ participants.js
@@ -11,7 +11,7 @@ window.HL_DEFAULT_PARTICIPANTS = [
   { address: "0x8dd9bef9f99750d9b9f8433a0032d3ba8a492121", name: "wmsooooj" },
   { address: "0xb325e9fe36c0ca5a0862551d0310217e381a029b", name: "hh" },
   { address: "0x7F8CF0EdCE270c61c6EabbAF81Ef910bbBcF205F", name: "0xCrescendo" },
-  { address: "0xdec8dea6c70e427fff0a4bb0b0b3de484b8d3763", name: "xx" },
+  { address: "0xE73C37068E678e889B343337e0aA28D1075C43AC", name: "xx" },
   { address: "0x42eB09A127301D5fB82647b80872e794c7930FFC", name: "是" },
   { address: "0x5d919b1c1f88443643f7152199d5fbe631246ca9", name: "song" },
   { address: "0x58d7670399b343a7bce263ce21704ba3a2879c63", name: "L神" },
